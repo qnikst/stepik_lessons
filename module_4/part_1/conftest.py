c9ea49f52ec40ca5376a26b237c1800b63b44c02
Alexander Vershilov <alexander.vershilov@gmail.com>
2021-06-05 09:13:22 +0300

    Cleanup configtest.py

--- module_4/part_1/conftest.py
+++ module_4/part_1/conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default="chrome",
+    parser.addoption('--language', action='store', default=None,
                      help="Choose language: ru, en, etc.")
 
 
@@ -16,14 +16,15 @@ def browser(request):
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
         options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        if not user_language is None:
+            options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
+        if not user_language is None:
+            fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
