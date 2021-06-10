c32e58520347db342a4376009972ee443b6dc95b
Alexander Vershilov <alexander.vershilov@gmail.com>
2021-06-10 23:51:02 +0300

    Add AAA

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -52,17 +52,21 @@ def test_add_button_has_correct_text_by_locale(browser, user_language):
     только локаль пользователя, которая передана через опции теста и мы не смотрим
     на локаль сайта.
     """
+    # Data
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     add_to_basket_button_selector = "button.btn-add-to-basket"
     if not user_language in LOCALE_MAPPINGS[user_language]:
         raise UnknownLanguage(user_language)
     expected_text = BASKET_MESSAGE_BY_SITE_LOCALE[LOCALE_MAPPINGS[user_language]]
 
+    # Arrange
     browser.get(link)
-
     button = browser.find_element_by_css_selector(add_to_basket_button_selector)
     current_text = button.text
 
+    # Act
+
+    # Assert
     assert current_text == expected_text, \
       f'Текст на копке должен быть {expected_text}, а там: {current_text}'
 
@@ -101,18 +105,22 @@ def test_add_button_has_correct_text_by_path(browser):
     сайта по локали браузера работает верно.
     Поэтому мы проверяем, что текст кнопки соотвествует локали сайта.
     """
+    # Data
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     add_to_basket_button_selector = "button.btn-add-to-basket"
 
+    # Arrange
     browser.get(link)
-
     button = browser.find_element_by_css_selector(add_to_basket_button_selector)
+    url_path = urllib.parse.urlparse(browser.current_url).path
+    # Act
+
     # Мы определяем язык по строке браузера, на странице, которой мы находися
     #  напр. http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/
     #                                               ^
     #                                               +--- русская локаль
     #
-    url_path = urllib.parse.urlparse(browser.current_url).path
+    # Assert
     locale = url_path.split(sep="/")[1]
     current_text = button.text
     if not locale in BASKET_MESSAGE_BY_SITE_LOCALE:
