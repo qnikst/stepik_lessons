9a1fb30fe84dcff74cf5c595ce10051aa8148317
Alexander Vershilov <alexander.vershilov@gmail.com>
2021-06-12 09:16:50 +0300

    [Module5] convert test to the class based one

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,10 +1,11 @@
+from .pages.main_page import MainPage
+
 link = "http://selenium1py.pythonanywhere.com/"
 
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
-
 class TestMainPage:
-    def test_guest_can_go_to_login_page(self, browser):
-        browser.get(link) 
-        go_to_login_page(browser) 
+
+    class TestMainPage:
+        def test_guest_can_go_to_login_page(self, browser):
+            page = MainPage(browser, link)
+            page.open()
+            page.go_to_login_page() 
