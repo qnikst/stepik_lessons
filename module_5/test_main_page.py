8c42953d661917ae847171bf194c87e5d43bbf21
Alexander Vershilov <alexander.vershilov@gmail.com>
2021-06-12 09:08:27 +0300

    [Module5] introduce BasePage class

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,7 +1,10 @@
 link = "http://selenium1py.pythonanywhere.com/"
 
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
 class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        browser.get(link)
-        login_link = browser.find_element_by_css_selector("#login_link")
-        login_link.click()
+        browser.get(link) 
+        go_to_login_page(browser) 
