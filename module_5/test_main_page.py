6fcd010f54451cc55c231ef0c3d06a925ce62457
Alexander Vershilov <alexander.vershilov@gmail.com>
2021-06-13 21:11:05 +0300

    [Module5] Add test grouping.

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -6,10 +6,22 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 class TestMainPage:
 
+
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_basket_page()
+        basket_page.should_be_empty()
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+
     def test_guest_can_go_to_login_page(self, browser):
         page = MainPage(browser, link)
         page.open()
-        page.go_to_login_page() 
+        page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
@@ -17,11 +29,3 @@ class TestMainPage:
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
-
-    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
-        page = MainPage(browser, link)
-        page.open()
-        page.go_to_basket_page() 
-        basket_page = BasketPage(browser, browser.current_url)
-        basket_page.should_be_basket_page()
-        basket_page.should_be_empty()
