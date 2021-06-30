197c5c7a1301f54394f28d24886e1299888ac0ab
Alexander Vershilov <alexander.vershilov@sirius.online>
2021-06-30 23:49:06 +0300

    [Final] Подготовлен репозиторий

--- final/conftest.py
+++ final/conftest.py
@@ -2,10 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-
 
 @pytest.fixture(scope="function")
 def browser(request):
