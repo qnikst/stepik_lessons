edc482f569fb6ee5df83fdd01ec5d63e1ce80ed5
Alexander Vershilov <alexander.vershilov@sirius.online>
2021-07-01 01:25:05 +0300

    Новая утилита find_price_at
    find_price_at позволяет искать цены в указанном
    элементе.

---
+++ final/utils/parse_page.py
@@ -0,0 +1,25 @@
+"""
+Набор вспомогательных функций для парсинга произвольной страницы
+сайта.
+"""
+import re
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+Locator = tuple[By, str]
+
+
+def find_price_at(where: WebElement, what: Locator):
+    """
+    Находит цену в указанном месте страницы сайта.
+    *N.B.* Функция не поддерживает локали и предполагает, что точка это разделитель
+    десятичных знаков и отсутствует разделитель разрядов
+
+    :param where: Объект, в контексте которого мы ищем цену
+    :param what:
+    :return:
+    """
+    price_text = where.find_element(*what).text
+    search_result = re.search(r"\d+.?\d*", price_text)
+    return float(search_result.group(0))
