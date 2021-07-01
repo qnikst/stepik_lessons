6d609bcf5554c4e4cac9ab1bec0ffa18774516f0
Alexander Vershilov <alexander.vershilov@sirius.online>
2021-07-01 04:03:49 +0300

    Remove garbage.

--- module_4/firefox.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
-driver = webdriver.Firefox()
-
-driver.get("https://stepik.org/lesson/25969/step/8")
